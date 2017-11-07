664735e727d9776fb8c9885698a713b9ede8c784
ShmvanHouten <svanhouten@gmail.com>
2017-11-07 21:56:38 +0100

    Made the calulator work for one and two steps.

--- src/main/kotlin/com/github/shmvanhouten/adventofcode/day1/HowFarIsEasterBunnyHqCalculator.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode/day1/HowFarIsEasterBunnyHqCalculator.kt
@@ -3,7 +3,8 @@ package com.github.shmvanhouten.adventofcode.day1
 
 class HowFarIsEasterBunnyHqCalculator(){
     fun calculateDistanceToHq(instructions: String): Int {
-        return 1
+        val instructionsList: List<String> = instructions.split(", ")
+        return instructionsList.sumBy { it.substring(1).toInt() }
     }
 
 }
--- src/test/kotlin/com/github/shmvanhouten/adventofcode/day1/HowFarIsEasterBunnyHqCalculatorSpec.kt
+++ src/test/kotlin/com/github/shmvanhouten/adventofcode/day1/HowFarIsEasterBunnyHqCalculatorSpec.kt
@@ -9,13 +9,27 @@ import org.jetbrains.spek.api.dsl.it
 
 
 class HowFarIsEasterBunnyHqCalculatorSpec : Spek({
-    describe("a calculator to calculate how far the EasterBunny's hq is"){
+    describe("a calculator to calculate how far the EasterBunny's hq is") {
         val calculator = HowFarIsEasterBunnyHqCalculator()
 
         given("We get the instructions: go 1 step to the right") {
             val distance: Int = calculator.calculateDistanceToHq("R1")
-            it("should calculate a distance of 1"){
+            it("should calculate a distance of 1") {
                 assertThat(distance, equalTo(1))
+            }
+        }
+
+        given("We get the instructions: go 2 steps to the right") {
+            val distance: Int = calculator.calculateDistanceToHq("R2")
+            it("should calculate a distance of 2") {
+                assertThat(distance, equalTo(2))
+            }
+        }
+
+        given("We get the instructions: go 1 step to the right, and another step to the right") {
+            val distance: Int = calculator.calculateDistanceToHq("R1, R1")
+            it("should calculate a distance of 2") {
+                assertThat(distance, equalTo(2))
             }
         }
     }
