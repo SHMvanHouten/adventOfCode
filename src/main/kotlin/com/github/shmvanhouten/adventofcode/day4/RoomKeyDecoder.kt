986233350657a9a44436de8ae6bae8ddd718fb33
SvanHouten <svanhouten@gmail.com>
2017-11-11 17:08:01 +0100

    Completed a functioning solution, unfortunately it doesn't quite match the question of the challenge, I overthought my solution but am committing it in case it might be useful for part 2...

--- src/main/kotlin/com/github/shmvanhouten/adventofcode/day4/RoomKeyDecoder.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode/day4/RoomKeyDecoder.kt
@@ -1,15 +1,33 @@
 package com.github.shmvanhouten.adventofcode.day4
 
 class RoomKeyDecoder {
-    fun checkIfRoomIsReal(key: String): Boolean {
-        val (name: List<String>, sectorId: Int, checkSum: String) = retrieveRoomIdentifiersFromKey(key)
-        return true
+
+    fun checkIfRoomsAreReal(keys: String): Int {
+        return keys.split("\n").sumBy { checkIfRoomIsReal(it.trim()) }
     }
 
-    private fun retrieveRoomIdentifiersFromKey(key: String): Triple<List<String>, Int, String> {
+    fun checkIfRoomIsReal(key: String): Int {
+        val (name: String, sectorId: Int, checkSum: String) = retrieveRoomIdentifiersFromKey(key)
+
+        var previousCheckSumCharValue = name.length + 1
+        var previousChar = '0'
+        for (char in checkSum) {
+            val amountOfTimesCharIsInName = name.count { it == char }
+            println("previousChar = $previousChar, char = $char, previousCheckSum = $previousCheckSumCharValue, amountOfTimesCharIsInName = $amountOfTimesCharIsInName")
+            if (amountOfTimesCharIsInName > previousCheckSumCharValue || (amountOfTimesCharIsInName == previousCheckSumCharValue && char < previousChar)) {
+                return 0
+            }
+            previousCheckSumCharValue = amountOfTimesCharIsInName
+            previousChar = char
+        }
+        return sectorId
+    }
+
+    private fun retrieveRoomIdentifiersFromKey(key: String): Triple<String, Int, String> {
         val (name, sectorIdAndCheckSum) = key.split("-").partition { it[0].isLetter() }
         val sectorId = sectorIdAndCheckSum[0].substringBefore('[').toInt()
         val checkSum = sectorIdAndCheckSum[0].substringAfter('[').substringBefore(']')
-        return Triple(name, sectorId, checkSum)
+        return Triple(name.joinToString(""), sectorId, checkSum)
     }
+
 }
