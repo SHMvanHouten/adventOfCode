b548baa79852e0cab50a30945e0589aed70f63a5
SjhmvanHouten <svanhouten@gmail.com>
2017-11-24 12:25:43 +0100

    switching computers, start of adding Dijkstra like algorithm

--- src/main/kotlin/com/github/shmvanhouten/adventofcode/day13/PathFinder.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode/day13/PathFinder.kt
@@ -2,6 +2,16 @@ package com.github.shmvanhouten.adventofcode.day13
 
 class PathFinder {
     fun findQuickestPathLength(maze: List<List<Int>>, targetCoordinate: Coordinate): Int {
-        return 0
+        val mazeWidth = maze.size
+        val mazeLength = maze[0].size
+        val unvisitedCoordinates = mutableSetOf<Coordinate>()
+
+        for (x in 0.until(mazeWidth)) {
+            for (y in 0.until(mazeLength)) {
+                unvisitedCoordinates.add(Coordinate(x, y))
+            }
+        }
+
+
     }
 }
