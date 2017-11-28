1c11d2686d26c8ff0f8deccb1793cafc129920b6
ShmvanHouten <svanhouten@gmail.com>
2017-11-28 21:42:05 +0100

    completed part 2 of day 15, it was super straight forward

--- src/test/kotlin/com/github/shmvanhouten/adventofcode/day15timingiseverything/CapsuleDropTimeFinderTest.kt
+++ src/test/kotlin/com/github/shmvanhouten/adventofcode/day15timingiseverything/CapsuleDropTimeFinderTest.kt
@@ -40,4 +40,23 @@ Disc #6 has 7 positions; at time=0, it is at position 0."""
 
         assertThat(finder.findTimeToPressButton(discMachine), equalTo(148737))
     }
+
+    @Test
+    fun `it should solve the challenge input part 2`() {
+        val finder = CapsuleDropTimeFinder()
+
+        val builder = DiscMachineBuilder()
+        val rawInput = """Disc #1 has 5 positions; at time=0, it is at position 2.
+Disc #2 has 13 positions; at time=0, it is at position 7.
+Disc #3 has 17 positions; at time=0, it is at position 10.
+Disc #4 has 3 positions; at time=0, it is at position 2.
+Disc #5 has 19 positions; at time=0, it is at position 9.
+Disc #6 has 7 positions; at time=0, it is at position 0."""
+
+        var discMachine = builder.build(rawInput)
+        discMachine = discMachine.addDisc(Disc(11, 0))
+
+        assertThat(finder.findTimeToPressButton(discMachine), equalTo(2353212))
+    }
+
 }
